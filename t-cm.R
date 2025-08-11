70abf40ffbab6d14f9eec0e9a0eccb9f1e08dfa4
bjungbogati <8600091+bjungbogati@users.noreply.github.com>
2025-08-11 07:04:09 +0545

    Added changes

---
+++ t-cm.R
@@ -0,0 +1,20 @@
+# *******************************************************************************************************
+# Program Name:                 t-cm.R
+# Project:                      TRA-025
+# Purpose:                      Create Table of CM
+# Original Author:              Binod Jung Bogati (linkedin.com/in/bjungbogati)
+# Copyright:                    © 2025. Unauthorized distribution or reuse prohibited.
+# Date Created:                 8/11/2025
+# Parameters:                   NA
+#
+# Input:                        
+# Output:                       
+# Modifications:
+#   Date        By             Changes
+# **********  *************  *************************************
+#   
+# ********************************************************************************************************/
+
+
+load(url("https://tinyurl.com/bp66vz5a"))
+
